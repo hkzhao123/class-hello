308ab046b4323d38309e03e3d34d352c77e84c44
hkzhao123 <1223602915@qq.com>
2018-06-21 14:48:16 +0800

    66

---
+++ helloclass.h
@@ -0,0 +1,15 @@
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+class helloclass{
+public:
+helloclass();
+~helloclass();
+void sayhello();
+
+};
+
+#endif
